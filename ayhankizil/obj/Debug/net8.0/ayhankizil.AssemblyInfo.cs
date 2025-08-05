a885b1f741515b20a1a8cadbfefa9c99c135352e
Esma Hatun Akbulut <153107265+Esmahtn@users.noreply.github.com>
2025-08-05 11:43:23 +0300

    foto yükleme kısmı tasarım düzenlendi

--- ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
+++ ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbbb46495036d3be40006120662afa93b1a8a384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444fc7d2b97a645e2dee67edc54c0e4cadde73b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
