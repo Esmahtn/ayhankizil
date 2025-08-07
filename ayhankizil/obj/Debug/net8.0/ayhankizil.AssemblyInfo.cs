4f02fcd8d79451066f76fa3dc838b9111ef0602b
Esma Hatun Akbulut <153107265+Esmahtn@users.noreply.github.com>
2025-08-07 11:24:48 +0300

    foto çok büyük açılıyor o ayarlanacak

--- ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
+++ ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1677b3f8fec95d6691ec2399938339802d85821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc87ec886fb6c8c01553aac49f756cc0bad16bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
