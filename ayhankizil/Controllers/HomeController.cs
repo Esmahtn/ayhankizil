4ae0de3302a0abb331b2cce472babcd3f76415dc
Esma Hatun Akbulut <153107265+Esmahtn@users.noreply.github.com>
2025-08-08 14:43:35 +0300

    paylaşılanalr ana sayfaya çekilde veritabanından sorunsuz alıyom

--- ayhankizil/Controllers/HomeController.cs
+++ ayhankizil/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 using System.Diagnostics;
 using ayhankizil.Models;
 using Microsoft.AspNetCore.Mvc;
+using ayhankizil.Data;  // DbContext namespace
+using System.Linq;
 
 namespace ayhankizil.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;  // DbContext
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Veritabanýndan paylaþýmlarý çek, ID'ye göre azalan sýrayla (yeni önce)
+            var paylasimlar = _db.Paylasimlar.OrderByDescending(p => p.Id).ToList();
+
+            return View(paylasimlar);  // View'e model olarak gönder
         }
 
         public IActionResult Privacy()
--- ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
+++ ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77775f1e9fab39398677fecbc80f8b0b4db2eded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eef1b941f8893808913ac62c606649e2f1b7311")]
 [assembly: System.Reflection.AssemblyProductAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
