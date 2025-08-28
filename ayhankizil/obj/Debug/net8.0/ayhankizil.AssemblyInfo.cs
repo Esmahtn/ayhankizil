3c78890f4f99649057fb8d2fe83cb08fb9eedd09
Esma Hatun Akbulut <153107265+Esmahtn@users.noreply.github.com>
2025-08-28 16:16:24 +0300

    .

--- ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
+++ ayhankizil/obj/Debug/net8.0/ayhankizil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af195352543d342d6a95006fd5f1c02d41082413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448774f6bd3fade313962fc4e95915fe21954db8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ayhankizil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
